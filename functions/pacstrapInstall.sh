099de0ae5cf1999fdaec4af42cd36667718ba369
hexadivine <hexadivine@gmail.com>
2025-07-12 01:55:14 +1000

    Enhance status messages in pacstrapInstall function for clarity during package installation

--- functions/pacstrapInstall.sh
+++ functions/pacstrapInstall.sh
@@ -4,16 +4,16 @@ function pacstrapInstall() {
     status "25"  "Installing required packages...\n\n[-] base\n[-] linux\n[-] linux-firmware\n[-] grub"
     pacstrap /mnt base &>/dev/null
     
-    status "40"  "Installing required packages...\n\n[+] base\n[-] linux\n[-] linux-firmware\n[-] grub"
+    status "40"  "Installing required packages (I am not crashed..relax..)\n\n[+] base\n[-] linux\n[-] linux-firmware\n[-] grub"
     pacstrap /mnt linux linux-firmware &>/dev/null
     
-    status "50"  "Installing required packages...\n\n[+] linux\n[+]  linux-firmware\n[-] grub\n[-] efibootmgr"
+    status "50"  "Installing required packages (See..I am running...)\n\n[+] linux\n[+]  linux-firmware\n[-] grub\n[-] efibootmgr"
     pacstrap /mnt grub efibootmgr &>/dev/null
     
-    status "60"  "Installing required packages...\n\n[+] grub\n[+] efibootmgr\n[-] networkmanager\n[-] vim"
+    status "60"  "Installing required packages (Trust the process...)\n\n[+] grub\n[+] efibootmgr\n[-] networkmanager\n[-] vim"
     pacstrap /mnt networkmanager &>/dev/null
     
-    status "65"  "Installing required packages...\n\n[+] efibootmgr\n[+] networkmanager\n[-] vim \n[-] nano"
+    status "65"  "Installing required packages (Almost finished...)\n\n[+] efibootmgr\n[+] networkmanager\n[-] vim \n[-] nano"
     pacstrap /mnt sudo vim nano &>/dev/null
     
     status "70"  "Base packages installed...\n\[+] efibootmgr\n[+] networkmanager\n[+] vim \n[+] nano"
